3b2004ac91eca1820aead5c685ca713e140260d6
manuel matias zubieta <manuelzubieta14@gmail.com>
2014-05-29 11:43:36 -0300

    first commit

---
+++ mavenproject1/mavenproject1-ejb/src/main/java/models/Cliente.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package models;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Administrator
+ */
+@Entity
+@Table(name = "Cliente")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Cliente.findAll", query = "SELECT c FROM Cliente c"),
+    @NamedQuery(name = "Cliente.findByIdCliente", query = "SELECT c FROM Cliente c WHERE c.idCliente = :idCliente"),
+    @NamedQuery(name = "Cliente.findByMail", query = "SELECT c FROM Cliente c WHERE c.mail = :mail"),
+    @NamedQuery(name = "Cliente.findByUsuario", query = "SELECT c FROM Cliente c WHERE c.usuario = :usuario"),
+    @NamedQuery(name = "Cliente.findByContrase\u00f1a", query = "SELECT c FROM Cliente c WHERE c.contrase\u00f1a = :contrase\u00f1a"),
+    @NamedQuery(name = "Cliente.findByCuit", query = "SELECT c FROM Cliente c WHERE c.cuit = :cuit"),
+    @NamedQuery(name = "Cliente.findByDni", query = "SELECT c FROM Cliente c WHERE c.dni = :dni"),
+    @NamedQuery(name = "Cliente.findByEdad", query = "SELECT c FROM Cliente c WHERE c.edad = :edad"),
+    @NamedQuery(name = "Cliente.findByNombre", query = "SELECT c FROM Cliente c WHERE c.nombre = :nombre")})
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idCliente")
+    private Integer idCliente;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "mail")
+    private String mail;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "usuario")
+    private String usuario;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "contrase\u00f1a")
+    private String contraseña;
+    @Size(max = 20)
+    @Column(name = "cuit")
+    private String cuit;
+    @Column(name = "Dni")
+    private Integer dni;
+    @Column(name = "Edad")
+    private Integer edad;
+    @Size(max = 30)
+    @Column(name = "Nombre")
+    private String nombre;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "cliente")
+    private Collection<ReservaPaquete> reservaPaqueteCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idCliente")
+    private Collection<FormadePagoReserva> formadePagoReservaCollection;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Cliente(Integer idCliente, String mail, String usuario, String contraseña) {
+        this.idCliente = idCliente;
+        this.mail = mail;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(String cuit) {
+        this.cuit = cuit;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @XmlTransient
+    public Collection<ReservaPaquete> getReservaPaqueteCollection() {
+        return reservaPaqueteCollection;
+    }
+
+    public void setReservaPaqueteCollection(Collection<ReservaPaquete> reservaPaqueteCollection) {
+        this.reservaPaqueteCollection = reservaPaqueteCollection;
+    }
+
+    @XmlTransient
+    public Collection<FormadePagoReserva> getFormadePagoReservaCollection() {
+        return formadePagoReservaCollection;
+    }
+
+    public void setFormadePagoReservaCollection(Collection<FormadePagoReserva> formadePagoReservaCollection) {
+        this.formadePagoReservaCollection = formadePagoReservaCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idCliente != null ? idCliente.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.idCliente == null && other.idCliente != null) || (this.idCliente != null && !this.idCliente.equals(other.idCliente))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.Cliente[ idCliente=" + idCliente + " ]";
+    }
+    
+}
---
+++ mavenproject1/mavenproject1-ejb/src/main/java/models/Transporte.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package models;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Administrator
+ */
+@Entity
+@Table(name = "Transporte")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Transporte.findAll", query = "SELECT t FROM Transporte t"),
+    @NamedQuery(name = "Transporte.findByIdTransporte", query = "SELECT t FROM Transporte t WHERE t.idTransporte = :idTransporte"),
+    @NamedQuery(name = "Transporte.findByTipoTransporte", query = "SELECT t FROM Transporte t WHERE t.tipoTransporte = :tipoTransporte")})
+public class Transporte implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idTransporte")
+    private Integer idTransporte;
+    @Size(max = 20)
+    @Column(name = "tipoTransporte")
+    private String tipoTransporte;
+    @JoinColumn(name = "idPaquete", referencedColumnName = "idPaquete")
+    @ManyToOne(optional = false)
+    private Paquete idPaquete;
+
+    public Transporte() {
+    }
+
+    public Transporte(Integer idTransporte) {
+        this.idTransporte = idTransporte;
+    }
+
+    public Integer getIdTransporte() {
+        return idTransporte;
+    }
+
+    public void setIdTransporte(Integer idTransporte) {
+        this.idTransporte = idTransporte;
+    }
+
+    public String getTipoTransporte() {
+        return tipoTransporte;
+    }
+
+    public void setTipoTransporte(String tipoTransporte) {
+        this.tipoTransporte = tipoTransporte;
+    }
+
+    public Paquete getIdPaquete() {
+        return idPaquete;
+    }
+
+    public void setIdPaquete(Paquete idPaquete) {
+        this.idPaquete = idPaquete;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idTransporte != null ? idTransporte.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Transporte)) {
+            return false;
+        }
+        Transporte other = (Transporte) object;
+        if ((this.idTransporte == null && other.idTransporte != null) || (this.idTransporte != null && !this.idTransporte.equals(other.idTransporte))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.Transporte[ idTransporte=" + idTransporte + " ]";
+    }
+    
+}
