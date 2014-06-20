9b426777c38e5ab5b4965e07edb49962f5059d08
Verónica <Vernica@190.183.91.132>
2014-06-20 16:59:23 -0300

    web service ObtenerPaquetes

--- mavenproject1/mavenproject1-ejb/src/main/java/dacs/dao/PaqueteFacade.java
+++ mavenproject1/mavenproject1-ejb/src/main/java/dacs/dao/PaqueteFacade.java
@@ -7,9 +7,14 @@
 package dacs.dao;
 
 import dacs.models.Paquete;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 /**
  *
@@ -27,4 +32,20 @@ public class PaqueteFacade extends AbstractFacade<Paquete> {
     public PaqueteFacade(){
         super(Paquete.class);
     }
-}
+    public List<Paquete> ListarPaquetes(){
+        //javax.persistence.Query p = getEntityManager().createQuery("select p from Paquete");
+        //List<Paquete> paquetes =  p.getResultList();
+        
+        CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+        
+        CriteriaQuery<Paquete> q2 = builder.createQuery(Paquete.class);
+        Root<Paquete> root = q2.from(Paquete.class);        
+        q2.select(root);
+        
+        TypedQuery<Paquete> pq = em.createQuery(q2);
+        
+              
+        return pq.getResultList();
+    }
+
+  }
